214bf5a8b32024a1e3ec59f6b1e5c6180f0cb3bb
priyanka_sahoo <priyanka.kundu.sahoo@gmail.com>
2026-04-20 16:57:26 -0400

    Added Practice.py file

---
+++ Practice.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+conn = mysql.connector.connect(host="localhost", user="root", passwd="1234", database="employee")
+
+mycursor = conn.cursor()
+
+mycursor.execute("insert into employee (name, EmployeeID, Salary)values(%s,%s,%s)", ('Ryan', 'E006', 65000.00))
+conn.commit()
+
