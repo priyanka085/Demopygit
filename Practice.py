c3b04b6aabb1557602963cc5387fecf58253adc2
priyanka_sahoo <priyanka.kundu.sahoo@gmail.com>
2026-04-21 15:57:22 -0400

    Delete Practice.py

--- Practice.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-conn = mysql.connector.connect(host="localhost", user="root", passwd="1234", database="employee")
-
-mycursor = conn.cursor()
-
-mycursor.execute("insert into employee (name, EmployeeID, Salary)values(%s,%s,%s)", ('Cott', 'E007', 67000.00))
-conn.commit()
-
